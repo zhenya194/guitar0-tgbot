48c36c254039f366de1059552af68259708217cc
zhenya194 <zhenya.pro1753@gmail.com>
2026-09-06 18:04:14 +0300

    Add json files of api

--- db/api.py
+++ db/api.py
@@ -1,13 +1,29 @@
+import aiofiles
 import aiohttp
+import json
 
 lessons_api_url: str = "https://api.guitar0.net/api/v1/lessons/"
 chords_api_url: str = "https://api.guitar0.net/api/v1/chords/"
 
 async def get_api_data():
     async with aiohttp.ClientSession() as session:
-        async with session.get(lessons_api_url, timeout=30) as response:
-            data_lessons = await response.json()
-        async with session.get(chords_api_url, timeout=30) as response:
-            data_chords = await response.json()
+        try:
+            async with session.get(lessons_api_url, timeout=30) as response:
+                data_lessons = await response.json()
+                async with aiofiles.open("db/api/lessons.json", "w", encoding="utf-8") as file:
+                    json_text = json.dumps(data_lessons, ensure_ascii=False, indent=4)
+                    await file.write(json_text)
+        except:
+            async with aiofiles.open("db/api/lessons.json", "r", encoding="utf-8") as file:
+                data_lessons = file
+        try:
+            async with session.get(chords_api_url, timeout=30) as response:
+                data_chords = await response.json()
+                async with aiofiles.open("db/api/chords.json", "w", encoding="utf-8") as file:
+                    json_text = json.dumps(data_chords, ensure_ascii=False, indent=4)
+                    await file.write(json_text)
+        except:
+            async with aiofiles.open("db/api/chords.json", "r", encoding="utf-8") as file:
+                data_chords = file
     all_data: list = [data_lessons, data_chords]
     return all_data
--- routers/base.py
+++ routers/base.py
@@ -28,11 +28,15 @@ async def cmd_lessons(message: types.Message, command: CommandObject):
     if not command.args:
         return await message.answer(f"Пожалуста, напишите номер урока после /lessons. Пример: <code>/lessons 18</code>", parse_mode="HTML")
     int_commands_arg_lessons: int = int(command.args)
+    songs_count = 0
 
     title: str = data_lessons["results"][int_commands_arg_lessons]["title"]
     video_url: str = data_lessons["results"][int_commands_arg_lessons]["video_url"]
-    song_0: str = data_lessons["results"][int_commands_arg_lessons]["songs"][0]["title"]
-    lessons_message = f"Ссылка на видео: {video_url}\n"
+    lessons_message: str = f"{title}\n\nСсылка на видео: {video_url}\n"
+    for i in range(len(data_lessons["results"][int_commands_arg_lessons]["songs"])):
+        songs_count += 1
+    for song in data_lessons["results"][int_commands_arg_lessons]["songs"]:
+        lessons_message += song["title"] + "\n"
     return message.answer(lessons_message, parse_mode="HTML")
 
 @router.message(Command("chords"))
@@ -40,13 +44,13 @@ async def cmd_lessons(message: types.Message, command: CommandObject):
     if not command.args:
         return await message.answer(f"Пожалуста, напишите номер акорда после /chords. Пример: <code>/chords 8</code>", parse_mode="HTML")
     int_commands_arg_chords: int = int(command.args) + 1
-    chords_message:str = f"Аккорды № {int_commands_arg_chords}\n\n\n"
+    chords_message: str = f"Аккорды № {int_commands_arg_chords}\n\n\n"
     for i in range(len(data_chords[int_commands_arg_chords]["positions"])):
                 chords_message += f"Аккорд {int_commands_arg_chords}.{i}:\n"
                 chords_message += f"Струна {data_chords[int_commands_arg_chords]["positions"][i]["string_number"]}\n"
                 chords_message += f"Лад {data_chords[int_commands_arg_chords]["positions"][i]["fret"]}\n"
                 chords_message += f"Палец {data_chords[int_commands_arg_chords]["positions"][i]["finger"]}\n\n"
-    return await message.answer(chords_message)
+    return message.answer(chords_message)
 
 @router.message(Command("help"))
 async def cmd_help(message: types.Message):
